ef678bc8ef54aaff0b2f3cb6bab1b65a6526c715
ioarun <arun_kumar271@hotmail.com>
2017-02-16 15:09:04 +0530

    Add final quiz stochastic motion

---
+++ lesson4/stochastic-motion.py
@@ -0,0 +1,83 @@
+# Write a function called stochastic_value that 
+# returns two grids. The first grid, value, should 
+# contain the computed value of each cell as shown 
+# in the video. The second grid, policy, should 
+# contain the optimum policy for each cell
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+
+def stochastic_value(grid,goal,cost_step,collision_cost,success_prob):
+    failure_prob = (1.0 - success_prob)/2.0 # Probability(stepping left) = prob(stepping right) = failure_prob
+    value = [[collision_cost for col in range(len(grid[0]))] for row in range(len(grid))]
+    policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+
+    change = True
+    while change == True:
+        change = False
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if [x, y] == goal:
+                    if value[x][y] > 0:
+                        value[x][y] = 0
+                        change = True
+                elif grid[x][y] == 0:
+                    for a in range(len(delta)):
+
+                        v2 = cost_step
+
+                        # explore outcomes
+                        for i in range(-1, 2):
+                            a2 = (a+i)%len(delta)
+                            x2 = x + delta[a2][0]
+                            y2 = y + delta[a2][1]
+
+                            if i == 0:
+                                prob = success_prob
+                            else:
+                                prob = failure_prob
+
+                            if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
+                                v2 += prob*value[x2][y2]
+                            else:
+                                v2 += prob*collision_cost
+
+                        if v2 < value[x][y]:
+                            print "original :",value[x][y], "changed :",v2
+                            value[x][y] = v2
+                            policy[x][y] = delta_name[a]
+                            change = True
+        #print policy
+    return value, policy
+
+grid = [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 1, 0]]
+goal = [0, len(grid[0])-1] # Goal is in top right corner
+cost_step = 1
+collision_cost = 100
+success_prob = 0.5
+
+value,policy = stochastic_value(grid,goal,cost_step,collision_cost,success_prob)
+for row in value:
+    print row
+for row in policy:
+    print row
+
+# Expected outputs:
+#
+# [57.9029, 40.2784, 26.0665,  0.0000]
+# [47.0547, 36.5722, 29.9937, 27.2698]
+# [53.1715, 42.0228, 37.7755, 45.0916]
+# [77.5858, 100.00, 100.00, 73.5458]
+#
+# ['>', 'v', 'v', '*']
+# ['>', '>', '^', '<']
+# ['>', '^', '^', '<']
+# ['^', ' ', ' ', '^']
